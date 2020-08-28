7f8ac943314e53c16a3b01c77d66ab1dc9f78850
sean <seangeekpro@gmail.com>
2020-08-28 09:47:13 +0700

    init + added /api/dev

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+// Define Routes
+app.use("/api/dev", require("./routes/dev"));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+module.exports = app;
