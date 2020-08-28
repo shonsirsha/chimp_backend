08dcec49ca81cc20a73f1ebf2eb62c128dc970b8
sean <seangeekpro@gmail.com>
2020-08-28 14:01:36 +0700

    created admin path to get all users

--- db/pool.js
+++ db/pool.js
@@ -6,3 +6,5 @@ const pool = new Pool({
   password: "123",
   port: 5432,
 });
+
+module.exports = pool;
---
+++ routes/admin.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const pool = require("../db/pool");
+const router = express.Router();
+
+router.get("/all-users", (req, res) => {
+  pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
+    if (error) {
+      res.status(400).json(error);
+    }
+    res.status(200).json(results.rows);
+  });
+});
+
+module.exports = router;
