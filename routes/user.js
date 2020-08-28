b2f7e7683ecd3dd055e9198062f5fc39131db648
sean <seangeekpro@gmail.com>
2020-08-28 20:10:37 +0700

    installed nodemon + finished get current user api

--- routes/user.js
+++ routes/user.js
@@ -11,8 +11,22 @@ const checkuserExists = require("./utils/checkUserExists");
 //@route    GET api/user
 //@desc     Get currently logged in user
 //@access   Private
-router.get("/", auth, (req, res) => {
+router.get("/", auth, async (req, res) => {
   const { user_uid } = req;
+
+  try {
+    let { rows } = await pool.query(
+      `SELECT * FROM users WHERE user_uid='${user_uid}'`
+    );
+    for (const property in rows[0]) {
+      if (property === "password" || property === "id") {
+        delete rows[0][property];
+      }
+    }
+    res.status(200).json({ user: rows[0], message: "success" });
+  } catch {
+    res.status(500).send("Server error");
+  }
 });
 
 module.exports = router;
