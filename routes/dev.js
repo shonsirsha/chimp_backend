0a236639eba49b5e71e6b1cfd41b9de5d1fcfe06
sean <seangeekpro@gmail.com>
2020-08-29 13:59:50 +0700

    rm logs

--- routes/dev.js
+++ routes/dev.js
@@ -2,7 +2,6 @@ const express = require("express");
 require("dotenv").config();
 const router = express.Router();
 router.get("/", (req, res) => {
-  console.log(process.env.HALU);
   return res.status(200).json({ message: "Hello World!" });
 });
 
