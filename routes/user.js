da6e382345a359279f7945f20f52c104fc051297
sean <seangeekpro@gmail.com>
2020-09-04 01:55:40 +0700

    update profile pic endpoint

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 const bcrypt = require("bcryptjs");
+const fs = require("fs");
 const jwt = require("jsonwebtoken");
+const path = require("path");
 const auth = require("../middleware/auth");
 const pool = require("../db/pool");
 const router = express.Router();
@@ -24,8 +26,78 @@ router.get("/", auth, async (req, res) => {
         delete rows[0][property];
       }
     }
-    res.status(200).json({ user: rows[0], msg: "success" });
+    if (rows[0] === null || rows[0] === undefined) {
+      // if token is valid but that uid not in db
+      res.status(401).json({ msg: "token is invalid" });
+    } else {
+      res.status(200).json({ user: rows[0], msg: "success" });
+    }
   } catch {
+    res.status(500).send("Server error");
+  }
+});
+
+//@route    PUT api/user
+//@desc     Update (currently logged in) user's profile detail
+//@access   Private
+router.put("/", auth, async (req, res) => {
+  const { user_uid } = req;
+  const { first_name, last_name } = req.body;
+  try {
+    pool.query(
+      `UPDATE user_profile SET first_name='${first_name}', last_name='${last_name}' WHERE user_uid='${user_uid}'`,
+      (err) => {
+        if (!err) {
+          res.status(200).json({ msg: "profile_detail_updated" });
+        } else {
+          res.status(400).json(error);
+        }
+      }
+    );
+  } catch (e) {
+    res.status(500).send("Server error");
+  }
+});
+
+//@route    PUT api/user/profile-picture
+//@desc     Update (currently logged in) user's profile picture
+//@access   Private
+router.put("/profile-picture", auth, async (req, res) => {
+  const { user_uid } = req;
+  if (req.files === null) {
+    return res.status(400).json({ msg: "file_not_found" });
+  }
+  const { file } = req.files;
+  try {
+    const fileExt = path.extname(file.name);
+    let newFileName =
+      file.name.substr(0, file.name.lastIndexOf(".")).replace(/ /g, "") +
+      Date.now() +
+      fileExt;
+    let dir = `user_uploads/public/images/profile_pictures/${user_uid}`;
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+    file.mv(`${dir}/${newFileName}`, (err) => {
+      if (err) {
+        return res.status(400).json({ msg: `mv_file_failed` });
+      }
+      pool.query(
+        `UPDATE user_profile SET profile_pic_url='${dir}' WHERE user_uid='${user_uid}'`,
+        (err) => {
+          if (!err) {
+            res.status(200).json({
+              msg: "profile_pic_updated",
+              filePath: `${process.env.SERVER_ADDRESS}/${dir}/${newFileName}`,
+            });
+          } else {
+            res.status(400).json(error);
+          }
+        }
+      );
+    });
+  } catch (e) {
+    console.log(e);
     res.status(500).send("Server error");
   }
 });
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const path = require("path");
+const fileUpload = require("express-fileupload");
 
 const app = express();
 
 // Init Middleware
 app.use(express.json({ extended: false }));
+app.use(fileUpload());
 
 // Define Routes
 app.use("/api/dev", require("./routes/dev"));
