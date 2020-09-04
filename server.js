ede68ae6df2214e5bb45b912beabbab008c746c5
sean <seangeekpro@gmail.com>
2020-09-04 14:35:57 +0700

    http server running

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.use(express.json({ extended: false }));
 app.use(fileUpload());
 
 // Define Routes
+
 app.use("/api/dev", require("./routes/dev"));
 app.use("/api/admin", require("./routes/admin"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/user", require("./routes/user"));
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 module.exports = app;
