74798ffb09cacf7cd4a7a6f48665ae212cd2bc9f
sean <seangeekpro@gmail.com>
2020-09-05 12:40:16 +0700

    tidy

--- routes/utils/generateAccessToken.js
+++ routes/utils/generateAccessToken.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const generateAccessToken = (payload) => {
-  return jwt.sign({ payload }, process.env.JWT_SECRET, { expiresIn: 20 });
+  return jwt.sign({ payload }, process.env.JWT_SECRET, { expiresIn: "15m" });
 };
 
 module.exports = generateAccessToken;
