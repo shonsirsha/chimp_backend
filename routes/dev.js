48a629e324fcbd195f9f17690bcb15b69fa1f65e
sean <seangeekpro@gmail.com>
2020-11-01 14:32:06 +0700

    using correct msg

--- routes/dev.js
+++ routes/dev.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const router = express.Router();
 router.get("/", (req, res) => {
-  return res.status(200).json({ msg: "Whatsgud  World!@!!!!!" });
+  return res.status(200).json({ msg: "Hello world" });
 });
 
 module.exports = router;
