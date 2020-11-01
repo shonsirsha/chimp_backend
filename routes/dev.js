7f5b87edb904d17a505d1233ce8e6e3521bc91c8
sean <seangeekpro@gmail.com>
2020-11-01 13:58:34 +0700

    changes

--- routes/dev.js
+++ routes/dev.js
@@ -2,7 +2,7 @@ const express = require("express");
 require("dotenv").config();
 const router = express.Router();
 router.get("/", (req, res) => {
-  return res.status(200).json({ msg: "Hello World!" });
+  return res.status(200).json({ msg: "Hello Worlds!@!!!!!" });
 });
 
 module.exports = router;
