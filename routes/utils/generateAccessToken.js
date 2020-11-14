0fcf992045857e1ca8ec2452b42a410f7d642d82
sean <seangeekpro@gmail.com>
2020-11-14 14:42:02 +0700

    using env to set JWT access token expr time

--- routes/utils/generateAccessToken.js
+++ routes/utils/generateAccessToken.js
@@ -1,6 +1,8 @@
 const jwt = require("jsonwebtoken");
 const generateAccessToken = (payload) => {
-  return jwt.sign({ payload }, process.env.JWT_SECRET, { expiresIn: "30m" });
+  return jwt.sign({ payload }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPR_TIME,
+  });
 };
 
 module.exports = generateAccessToken;
