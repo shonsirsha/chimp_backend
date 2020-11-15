c862f42dda35ddf065d70851bc6fb1d97ea6d547
sean <seangeekpro@gmail.com>
2020-11-15 14:24:08 +0700

    distributed logging for infos and token error events

--- middleware/loggers/auth/failed.js
+++ middleware/loggers/auth/failed.js
@@ -1,6 +1,8 @@
 const { tokenErrorLog } = require("../logger");
 module.exports = function (req, violation) {
   const { ip, baseUrl, method } = req;
-  const str = `${ip} | ${violation} on ${baseUrl} | ${method}`;
+  const d = new Date();
+  const n = d.toISOString();
+  const str = `${n} | ${violation} on ${baseUrl} | ${method} | platform ${process.platform} on address ${ip}`;
   tokenErrorLog.token_error(str);
 };
