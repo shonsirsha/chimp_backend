442203672e30e0bf14071fdda4e106234a698024
sean <seangeekpro@gmail.com>
2020-11-16 19:46:08 +0700

    new postman collection and whole schema sql script added

--- routes/dev.js
+++ routes/dev.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  return res.status(200).json({ msg: `Hello world! ${process.env.NODE_ENV}` });
+  return res
+    .status(200)
+    .json({ msg: `Hello world! ${process.env.DB_DATABASE_NAME}` });
 });
 
 module.exports = router;
