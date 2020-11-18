3011d032ee20a1ba1aae1f7efa5bffb3939ce48a
sean <seangeekpro@gmail.com>
2020-11-18 14:33:50 +0700

    instances num changed

--- test/auth.js
+++ test/auth.js
@@ -1,30 +1,20 @@
 const request = require("supertest");
 const authapp = require("../authServer.js");
-const pool = require("../db/pool");
 let envpath;
 if (process.env.NODE_ENV === "development") {
   envpath = "./.env";
 } else if (process.env.NODE_ENV == "production") {
-  envpath = "../env/.env";
+  envpath = "../../env/.env";
 } else if (process.env.NODE_ENV === "test-production") {
-  envpath = "../env/.test.env";
+  envpath = "../../env/.test.env";
 } else if (process.env.NODE_ENV === "test-development") {
   envpath = "./.test.env";
 }
 require("dotenv").config({
   path: envpath,
 });
+const pool = require("../db/pool");
 describe("Auth tests", function () {
-  it("succeeded signing up", function (done) {
-    request(authapp)
-      .post("/api/auth/sign-up")
-      .send({
-        email: "sean@2mail.com",
-        password: "123456",
-      })
-      .expect(200, done);
-  });
-
   it("failed signing up", function (done) {
     request(authapp)
       .post("/api/auth/sign-up")
@@ -53,8 +43,4 @@ describe("Auth tests", function () {
       })
       .expect(401, done);
   });
-
-  pool.query(
-    `truncate table companies, company_contact, contacts, tag_contact, tokens, user_profile, companies, users RESTART identity`
-  );
 });
