09dd55d04e721eefca462f4992b9f3f24ee01ed0
sean <seangeekpro@gmail.com>
2020-11-18 15:45:49 +0700

    using different ports for tests

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use("/api/contacts", require("./routes/contacts"));
 app.use("/api/company", require("./routes/company"));
 app.use("/api/companies", require("./routes/companies"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.REST_PORT || 5000;
 app.listen(PORT, () => {
   serverInfo.info(`REST API server started on port ${PORT}`);
 });
