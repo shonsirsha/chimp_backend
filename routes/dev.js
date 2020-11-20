dd59819c62c040c236e9b4f42a9d78f70094ad1c
sean <seangeekpro@gmail.com>
2020-11-20 09:50:04 +0700

    created test route

--- routes/dev.js
+++ routes/dev.js
@@ -5,4 +5,8 @@ router.get("/", (req, res) => {
   return res.status(200).json({ msg: `Hello world! ${process.env.NODE_ENV}` });
 });
 
+router.get("/2", (req, res) => {
+  return res.status(200).json({ msg: `Hello hello` });
+});
+
 module.exports = router;
