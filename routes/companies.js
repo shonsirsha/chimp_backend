c07718a7b7a6f6473ba4279a6ab9eb1a300fd0ce
sean <seangeekpro@gmail.com>
2020-12-04 21:38:33 +0100

    return empty array for /companies (GET) on no companies at all

--- routes/companies.js
+++ routes/companies.js
@@ -25,39 +25,43 @@ router.get("/", auth, async (req, res) => {
         },
       });
       //loop thru fetched companies
-      companies.map(async (company, ix) => {
-        let company_contact_arr = [];
-        let companyObj = company.dataValues;
+      if (companies.length > 0) {
+        companies.map(async (company, ix) => {
+          let company_contact_arr = [];
+          let companyObj = company.dataValues;
 
-        //give picture a full url if exists
-        if (companyObj.picture !== "") {
-          let dir = `${process.env.USER_UPLOAD_COMPANY_IMAGE}${company.dataValues.company_uid}`;
-          companyObj.picture = `${process.env.FILE_SERVER_HOST}/${dir}/${companyObj.picture}`;
-        }
+          //give picture a full url if exists
+          if (companyObj.picture !== "") {
+            let dir = `${process.env.USER_UPLOAD_COMPANY_IMAGE}${company.dataValues.company_uid}`;
+            companyObj.picture = `${process.env.FILE_SERVER_HOST}/${dir}/${companyObj.picture}`;
+          }
 
-        //get all company contact (getting all contacts for this company)
-        const company_contact = await CompanyContact.findAll({
-          where: {
-            company_uid: company.dataValues.company_uid,
-          },
-          attributes: ["contact_uid"],
+          //get all company contact (getting all contacts for this company)
+          const company_contact = await CompanyContact.findAll({
+            where: {
+              company_uid: company.dataValues.company_uid,
+            },
+            attributes: ["contact_uid"],
+          });
+
+          //loop thru fetched cC
+          company_contact.map((cc) => {
+            company_contact_arr.push(cc.dataValues.contact_uid);
+          });
+
+          //setting all contacts that works for this company (cC)
+          companyObj["people"] = company_contact_arr;
+
+          companiesArr.push(companyObj);
+          if (ix === companies.length - 1) {
+            return res
+              .status(200)
+              .json({ msg: "success", companies: companiesArr });
+          }
         });
-
-        //loop thru fetched cC
-        company_contact.map((cc) => {
-          company_contact_arr.push(cc.dataValues.contact_uid);
-        });
-
-        //setting all contacts that works for this company (cC)
-        companyObj["people"] = company_contact_arr;
-
-        companiesArr.push(companyObj);
-        if (ix === companies.length - 1) {
-          return res
-            .status(200)
-            .json({ msg: "success", companies: companiesArr });
-        }
-      });
+      } else {
+        return res.status(200).json({ msg: "success", companies: [] });
+      }
     } catch (e) {
       return res.status(400).json({ msg: e.name });
     }
